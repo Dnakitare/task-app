c9608774f515cc80350cc2da07a5026549d452a8
Daniel Nakitare <dnakitare@gmail.com>
2024-06-06 19:32:40 -0600

    chore: Refactor ProjectController@index to filter projects by name and paginate results

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Models\Project;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', 'App\Http\Controllers\AuthController@register');
-Route::get('/verify-email', 'App\Http\Controllers\AuthController@verifyEmail')->name('verify-email');
-Route::post('/login', 'App\Http\Controllers\AuthController@login');
+Route::middleware('auth:sanctum')->group(function () {
+  Route::post('/register', 'App\Http\Controllers\AuthController@register');
+  Route::get('/verify-email', 'App\Http\Controllers\AuthController@verifyEmail')->name('verify-email');
+  Route::post('/login', 'App\Http\Controllers\AuthController@login');
+});
 
-// Route::middleware('auth:sanctum')->group(function () {
+// use sanctum middleware to protect the routes
 Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
-Route::post('/projects', 'App\Http\Controllers\ProjectController@store');
-Route::get('/projects', 'App\Http\Controllers\ProjectController@index');
-Route::get('/projects/{project}', 'App\Http\Controllers\ProjectController@show');
-Route::put('/projects/{project}', 'App\Http\Controllers\ProjectController@update');
-Route::delete('/projects/{project}', 'App\Http\Controllers\ProjectController@destroy');
-Route::post('/projects/{project}/pin-on-dashboard', 'App\Http\Controllers\ProjectController@pinOnDashboard');
-Route::post('/projects/{project}/unpin-from-dashboard', 'App\Http\Controllers\ProjectController@unpinFromDashboard');
-// });
+
+Route::controller(ProjectController::class)->group(function () {
+  Route::post('/projects/{project}/pin-on-dashboard', 'pinOnDashboard');
+  Route::post('/projects/{project}/unpin-from-dashboard', 'unpinFromDashboard');
+  Route::post('/projects', 'store');
+  Route::get('/projects', 'index');
+  Route::get('/projects/{project}', 'show');
+  Route::put('/projects/{project}', 'update');
+  Route::delete('/projects/{project}', 'destroy');
+  Route::post('/projects/{project}/pin-on-dashboard', 'pinOnDashboard');
+  Route::post('/projects/{project}/unpin-from-dashboard', 'unpinFromDashboard');
+});
