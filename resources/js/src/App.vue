743b9ba3e186864f97bf90ba0d4b9227c275178f
Daniel Nakitare <dnakitare@gmail.com>
2024-06-14 13:05:16 -0600

    refactor: Update app.js to use app.ts for Vue 3 setup

---
+++ resources/js/src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const count = ref<number>(0)
+</script>
+
+<template>
+  <div class="container">
+    <h1>This is working - Vue 3</h1>
+    {{ count }}
+
+    <button type="button" @click="count++">Increment</button>
+    <RouterView/>
+  </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return redirect('/app/login');
+});
+
+Route::get('/app/{any}', function () {
     return view('welcome');
 });
